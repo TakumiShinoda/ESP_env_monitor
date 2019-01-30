b8a961d288dbe37af134df31e4a313c247b06919
TakimiShinoda <godgodx4@gmail.com>
2019-01-30 16:23:33 +0900

    update Dashboard

--- computer_client/src/assets/typescript/setting/components/SettingField/Dashboard.ts
+++ computer_client/src/assets/typescript/setting/components/SettingField/Dashboard.ts
@@ -4,7 +4,8 @@ import FieldInterface from './FieldInterface'
 const Styles: any = {
   'default': {
     '#dashboardField': {
-      'margin': '0 10%'
+      'margin': '0 10%',
+      'padding-top': '10px'
     },
     '#statusField': {
       'border': 'solid 1px',
@@ -16,7 +17,25 @@ const Styles: any = {
     '#statusList': {
       'font-size': '20px',
       'padding': '0 8px',
-      'list-style': 'none'
+      'list-style': 'none',
+    },
+    '#overviewField': {
+      'border': 'solid 1px',
+      'border-radius': '8px',
+      'width': '95%',
+      'margin': 'auto',
+      'padding': '0',
+    },
+    '#overviewList': {
+      'font-size': '20px',
+      'padding': '0 8px',
+      'list-style': 'none',
+      'margin-bottom': '0',
+    },
+    '#overviewListLastUpdate': {
+      'font-size': '12px',
+      'text-align': 'right',
+      'margin-top': '10px',
     }
   }
 }
@@ -32,11 +51,22 @@ export default class Dashboard implements FieldInterface{
 
   private statusList(): string{
     let ul: JQuery<HTMLElement> = $('<ul id=statusList>')
-    let list: string[] = ['WiFi', 'BLE']
+    let list: string[] = ['WiFi Power On', 'LAN Connection', 'BLE Connection']
 
     for(var i = 0; i < list.length; i++){
       ul.append($('<li>').append(list[i] + ': false'))
     }
+    return Utils.convertToHTML(ul)
+  }
+
+  private overviewList(): string{
+    let ul: JQuery<HTMLElement> = $('<ul id=overviewList>')
+    let list: string[] = ['Temperature', 'Humidity', 'Pressure']
+
+    for(var i = 0; i < list.length; i++){
+      ul.append($('<li>').append(list[i] + ': false'))
+    }
+    ul.append($('<li id=overviewListLastUpdate>').append('LastUpdate: 2019/1/30/16:19:25'))
     return Utils.convertToHTML(ul)
   }
 
@@ -53,6 +83,10 @@ export default class Dashboard implements FieldInterface{
         <div id=statusField>
           ${this.statusList()}
         </div>
+        <h2 style="margin-top: 25px;">Overview</h2>
+        <div id=overviewField>
+          ${this.overviewList()}
+        </di>
       </div>
     `
   }
