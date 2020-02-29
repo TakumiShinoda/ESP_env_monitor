c60b970d1d92e26a90b82aacd75026d1bb54c23e
TakumiShinoda <tshinoda@TakumiShinodanoMacBook-Pro.local>
2020-03-01 06:51:43 +0900

    remove waste characteristics

--- src/main.cpp
+++ src/main.cpp
@@ -57,13 +57,7 @@ float InternalTemp = 0;
 BLEServer *pServer;
 BLEService *pService;
 BLEService *debugService;
-BLECharacteristic *pCharacteristic;
-BLECharacteristic *wifiConfigPostCharacteristic;
-BLECharacteristic *wifiStatusCharacteristic;
-BLECharacteristic *wifiAPConnectCharacteristic;
-BLECharacteristic *wifiAPDisconnectCharacteristic;
-BLECharacteristic *getWifiAPConfigCharacteristic;
-BLECharacteristic *sensorPostCharacteristic;
+BLECharacteristic *commandCharacteristic;
 ServerObject Server;
 ESPIFFS espiffs;
 RTC rtc;
@@ -85,47 +79,7 @@ class MyServerCallbacks: public BLEServerCallbacks {
   }
 };
 
-class WifiAPConnectCharacteristicCallbacks: public BLECharacteristicCallbacks{
-  void onWrite(BLECharacteristic *chara){
-    std::vector<String> aroundSSIDs = getAroundSSIDs();
-    std::string rxVal = chara->getValue();
-    String rxString = utils.stdString2String(rxVal);
-    String savedWificonfs = espiffs.readFile(WIFI_CONF_DIR);
-    std::vector<String> splitSavedWificonfs = utils.split2vector(savedWificonfs, '\n');
-
-    if(rxVal == BLE_WIFI_CONNECT_PERMMISION){
-      for(String aroundSSID : aroundSSIDs){
-        for(String savedWificonf : splitSavedWificonfs){
-          String savedSSID = utils.split(savedWificonf, ',', 0);
-
-          if(aroundSSID == savedSSID){
-            String savedPASS = utils.split(savedWificonf, ',', 1);
-            char ssidBuff[255];
-            char passBuff[255];
-
-            savedSSID.toCharArray(ssidBuff, savedSSID.length() + 1);
-            savedPASS.toCharArray(passBuff, savedPASS.length() + 1);
-            if(connectAP(ssidBuff, passBuff)) Serial.println("WiFi Connected");
-            else Serial.println("WiFi Connection failed");
-            break;
-          }
-        }
-        if(WiFi.status() == WL_CONNECTED) break;
-      }
-    }
-  }
-};
-
-class WifiAPDisconnectCharacteristicCallbacks: public BLECharacteristicCallbacks{
-  void onWrite(BLECharacteristic *chara){
-    std::string rxVal = chara->getValue();
-    String rxString = utils.stdString2String(rxVal);
-
-    if(rxString == BLE_WIFI_DISCONNECT_PERMMISION) WiFi.disconnect();
-  }
-};
-
-class WifiConfigPostCharacteristicCallbacks: public BLECharacteristicCallbacks{
+class CharacteristicCallbacksCommand: public BLECharacteristicCallbacks{
   void onWrite(BLECharacteristic *pCharacteristic) {
     std::string rxValue = pCharacteristic->getValue();
     String rxString = utils.stdString2String(rxValue);
@@ -147,69 +101,6 @@ class WifiConfigPostCharacteristicCallbacks: public BLECharacteristicCallbacks{
     Serial.println(espiffs.readFile(WIFI_CONF_DIR));
   }
 };
-
-String makeResult_debug(){
-  String result = "";
-  float tempAve = 0;
-  float tempCnt = 0;
-  float tempSum = 0;
-
-  if(!std::isnan(EnvBuff[0]) && !std::isnan(EnvBuff[1]) && !std::isnan(EnvBuff[2])){
-    for(int i = 0; i < TEMP_HISTORY_LENGTH; i++){
-      if(TempHistory[i] != 0 && !std::isnan(TempHistory[i])){
-        tempSum += TempHistory[i];
-        tempCnt += 1;
-      }
-    }
-    tempAve = tempSum / tempCnt;
-
-    result += "Temp_Raw: ";
-    result += String(EnvBuff[0]);
-    result += "   Temp_Ave: ";
-    result += String(tempAve);
-    result += "   Temp_Adj_Simple: ";
-    result += String(EnvBuff[0] - (InternalTemp * TEMP_ADJ_RATIO));
-    result += "   Temp_Ave_Adj_Simple: ";
-    result += String(tempAve - (InternalTemp * 0.083));
-    result += "   Temp_CPU: ";
-    result += String(InternalTemp);
-    result += "   Humidity: ";
-    result += String(EnvBuff[1]);
-    result += "   Pressure: ";
-    result += String(EnvBuff[2]);
-
-    return result;
-  }else{
-    return "";
-  }
-}
-
-String makeResult(){
-  String result = "";
-  float tempAve = 0;
-  float tempCnt = 0;
-  float tempSum = 0;
-
-  if(!std::isnan(EnvBuff[0]) && !std::isnan(EnvBuff[1]) && !std::isnan(EnvBuff[2])){
-    for(int i = 0; i < TEMP_HISTORY_LENGTH; i++){
-      if(TempHistory[i] != 0 && !std::isnan(TempHistory[i])){
-        tempSum += TempHistory[i];
-        tempCnt += 1;
-      }
-    }
-    tempAve = tempSum / tempCnt;
-
-    result += String(EnvBuff[0]) + ','; // raw
-    result += String(tempAve)  + ','; // raw ave
-    result += String(EnvBuff[0] - (InternalTemp * TEMP_ADJ_RATIO)) + ','; // simple adj
-    result += String(tempAve - (InternalTemp * 0.083))  + ','; // simple adj ave
-    result += String(InternalTemp) + ','; // cpu temp
-    result += String(EnvBuff[1]) + ','; // humidity
-    result += String(EnvBuff[2]); // pressure
-
-    return result;
-  }else return "";
-}
 
 void homePageCallback(ChainArray params, String *resp, WiFiClient *client){
   *(resp) = ResultStr;
@@ -250,19 +141,11 @@ void setup(){
     pServer->setCallbacks(new MyServerCallbacks());
 
     // create BLE characteristic
-    pCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
-    wifiStatusCharacteristic = pService->createCharacteristic(WiFI_STATUS_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
-    wifiConfigPostCharacteristic = pService->createCharacteristic(WIFI_CONFIG_POST_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
-    wifiAPConnectCharacteristic = pService->createCharacteristic(WIFI_AP_CONNECT_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
-    wifiAPDisconnectCharacteristic = pService->createCharacteristic(WIFI_AP_DISCONNECT_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_WRITE);
-    getWifiAPConfigCharacteristic = debugService->createCharacteristic(GET_WIFI_AP_CONFIG_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
-    sensorPostCharacteristic = pService->createCharacteristic(SENSOR_POST_CHARACTERISTIC_UUID, BLECharacteristic::PROPERTY_READ);
+    commandCharacteristic = pService->createCharacteristic(CHARACTERISTIC_UUID_COMMAND, BLECharacteristic::PROPERTY_WRITE);
 
     // set BLE characteristic callbacks
-    wifiConfigPostCharacteristic->setCallbacks(new WifiConfigPostCharacteristicCallbacks());
-    wifiAPConnectCharacteristic->setCallbacks(new WifiAPConnectCharacteristicCallbacks());
-    wifiAPDisconnectCharacteristic->setCallbacks(new WifiAPDisconnectCharacteristicCallbacks());
-
+    commandCharacteristic->setCallbacks(new CharacteristicCallbacksCommand());
+    
     pService->start();
     debugService->start();
 
@@ -293,36 +176,8 @@ void loop(){
     getBME280Data(EnvBuff);
     InternalTemp = temperatureRead();
     struct tm *timeNow = rtc.now();
-    char resultCharBuff[512];
-    char resultDebugCharBuff[512];
-    char wifiAPConfigCharBuff[1024];
     std::string manufacturerData = "";
-    uint8_t resultDebugUint8Buff[512];
-    uint8_t resultUint8Buff[512];
-    uint8_t wifiStatusResult[8];
-    uint8_t wifiAPConfigUint8_tBuff[1024];
     struct FullInfo currentInfo;
-
-    utils.slideRightBuff(TempHistory, TEMP_HISTORY_LENGTH);
-    TempHistory[0] = EnvBuff[0];
-    ResultDebugStr = makeResult_debug() != "" ? makeResult_debug() : ResultDebugStr;
-    ResultStr = makeResult() != "" ? makeResult() : ResultStr;
-
-    // Serial.println(ResultDebugStr);
-    ResultDebugStr.toCharArray(resultDebugCharBuff, 512);
-    utils.charArrToUint8_tArr(resultDebugCharBuff, resultDebugUint8Buff, ResultDebugStr.length());
-
-    ResultStr.toCharArray(resultCharBuff, 512);
-    utils.charArrToUint8_tArr(resultCharBuff, resultUint8Buff, ResultStr.length());
-
-    WifiApConfigReserved.toCharArray(wifiAPConfigCharBuff, 1024);
-    utils.charArrToUint8_tArr(wifiAPConfigCharBuff, wifiAPConfigUint8_tBuff, WifiApConfigReserved.length());
-    getWifiAPConfigCharacteristic->setValue(wifiAPConfigUint8_tBuff, WifiApConfigReserved.length());
-
-    sensorPostCharacteristic->setValue(resultUint8Buff, ResultStr.length());
-
-    pCharacteristic->setValue(resultDebugUint8Buff, ResultDebugStr.length());
-    pCharacteristic->notify();
 
     currentInfo.temperature = EnvBuff[0];
     currentInfo.humidity = EnvBuff[1];
